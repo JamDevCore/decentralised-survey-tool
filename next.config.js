54343a81946e6fbc338bfaafa7404945b038b2d9
James Harding <jamesvitaly1993@gmail.com>
2022-12-13 13:46:26 +0000

    Removed unused TS configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
 
-module.exports = withNextra()
+module.exports = {
+  exportTrailingSlash: true,
+};
